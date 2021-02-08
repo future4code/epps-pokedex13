beb3321e16d238efcf7d1ff5283b305d230acb02
unknown <email-que-voce-usa-no-github>
2021-02-08 15:05:19 -0300

    Início - criação do Router

---
+++ pokedex13/src/routes/Router.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Home from '../pages/Home/Home'
+import Pokedex from '../pages/Pokedex/Pokedex'
+import Details from '../pages/Details/Details'
+import Error from '../pages/Error/Error'
+
+
+
+const Router = () => {
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        
+        <Route exact path={'/'}>
+          <Home />
+        </Route>
+
+        <Route exact path={'/pokedex'}>
+          <Pokedex />
+        </Route>
+
+        <Route exact path={'/details'}>
+          <Details />
+        </Route>
+
+        <Route>
+          <Error />
+        </Route>
+
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
---
+++ pokedex13/src/routes/coordinator.js
@@ -0,0 +1,13 @@
+export const goToHome = (history) => {
+  history.push('/')
+}
+
+export const goToPokedex = (history) => {
+  history.push('/pokedex')
+}
+
+export const goToDetails = (history) => {
+  history.push('/details')
+}
+
+
