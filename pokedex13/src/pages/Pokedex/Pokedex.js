cbd10d29f9d876e2db37871c313122f71f15e14d
unknown <email-que-voce-usa-no-github>
2021-02-10 11:39:12 -0300

    Pokedex - item chumbado, sem estilização e nenhum funcionamento ainda

--- pokedex13/src/pages/Pokedex/Pokedex.js
+++ pokedex13/src/pages/Pokedex/Pokedex.js
@@ -1,11 +1,46 @@
 import React from 'react'
+import styled from 'styled-components'
+const ContainerPokedex = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
 
+  
+`
 
+const ImagePokedex = styled.image`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  margin: auto;
+  padding: 1em;
+  grid-gap: 20px;
+  align-items: center;
+  flex-grow: 1;
+  justify-content: center;
+`
+const Button = styled.button`
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  width: 120px;
+  padding: 5px;
+  border-radius: 5px;   
+`
 const Pokedex = () => {
     
   return (
     <div>
       <h1>Pokedex</h1>
+      <ContainerPokedex>
+        <ImagePokedex>
+          <img src ={"https://picsum.photos/200/300"} />
+       
+         
+          
+        </ImagePokedex>
+        <Button>Remover</Button>
+        <Button>Ver detalhes</Button>
+      </ContainerPokedex>
     </div>
     )
 }
